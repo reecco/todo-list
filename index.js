bec3de1ec9a9212d866196953dcc2150b991de00
reecco <fredcardoso01@hotmail.com>
2022-12-13 15:11:50 -0300

    feat: add middleware cors

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv'
 
 import routes from './src/routes/index.js'
 import connection from './src/db/connection.js'
+import cors from './src/middlewares/cors.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 9000
 
-connection()
-
+cors(app)
 routes(app)
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+  connection()
+})
---
+++ src/middlewares/cors.js
@@ -0,0 +1,11 @@
+import cors from 'cors'
+
+export default (app) => {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
+    res.header("Access-Control-Allow-Methods", 'PUT,POST,DELETE')
+    app.use(cors())
+    next()
+  })
+}
