7996b5846db57aa3046ebb8135b0c16d9c14b351
reecco <fredcardoso01@hotmail.com>
2022-12-15 08:17:15 -0300

    feat: add environment variable session

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
 import express from "express"
 import session from 'express-session'
+import dotenv from 'dotenv'
 
 import user from './userRoutes.js'
 import task from './taskRoutes.js'
 
+dotenv.config()
+
 const routes = (app) => {
+  const secret = process.env.SECRET_SESSION
+
   app.route('/').get((req, res) => {
-    res.status(200).json({ message: 'Welcome to To-Do List', status: 200 })
+    res.status(200).json({ message: 'Welcome to the To-Do List', status: 200 })
   })
 
   app.use(
     express.json(),
-    session({ secret: 'teste', resave: false, saveUninitialized: false }),
+    session({ secret: secret, resave: false, saveUninitialized: false }),
     user,
     task
   )
